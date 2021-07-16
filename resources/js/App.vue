0d9c98d945036357db04d4eb97d0e9ca292f0193
wenxuanjun <wendster@163.com>
2021-07-16 15:11:07 +0800

    Fix: Wrong Code

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,15 +1,37 @@
 <template>
-<v-app>
-  <v-navigation-drawer app>
-  </v-navigation-drawer>
-
-  <v-app-bar app>
-  </v-app-bar>
-    <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-  <v-main>
-    <v-container fluid>
-      <!--<router-view></router-view>-->
-    </v-container>
-  </v-main>
-</v-app>
+  <v-app>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    </v-app-bar>
+    <v-navigation-drawer app v-model="drawer">
+      <v-list nav dense>
+        <v-list-item link>
+          <v-list-item-title>Foo</v-list-item-title>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-title>Bar</v-list-item-title>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-title>Shit</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <v-container fluid>
+        <!--<router-view></router-view>-->
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
+<script>
+export default {
+  data: () => ({
+    drawer: false,
+  }),
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
+};
+</script>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,10 +3,10 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
+        <title>Wonmega</title>
     </head>
     <body>
-        <div  id="app"></div>
-        <script src="{{ asset('js/app.js') }}"></script>
+        <div id="app"></div>
+        <script src="{{ mix('js/app.js') }}"></script>
     </body>
 </html>
